dc570487b2c1cfeb849d33e254feb217c5285003
TStavale <tassiostavale@hotmail.com>
2022-11-12 17:05:01 -0300

    No javacript criei a função para trocar a cor do circulo por trás da img grande do copo, quando selecionado pelas imagens menores.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,13 @@ let image = document.querySelector(".starbucks"); /* Variavel para capturar a im
 let circle = document.querySelector(".circle"); /* Variavel para capturar o circulo por trás da img grande, para troca de cor.*/
 
 function changeImage(endereco) {
-    image.src = endereco
-    circle.style.backgound = color
+    image.src = endereco /* Aqui esstou pegando a variavel que está com a imagems guardada e informo que quero trocar ela, 
+                            como ela esta no html, tenho que imformar o onde quer mexer nele (.src). */
 }
 
+function changeColor(color) {
+    circle.style.background = color /* Aqui esstou pegando a variavel que está com a cor guardada e informo que quero trocar ela, 
+                                    como ela esta no css, tenho que imformar que é nele que quero mexer (.stylo) e o que quero mecher 
+                                    dentro da classe(background). */
+}
 
